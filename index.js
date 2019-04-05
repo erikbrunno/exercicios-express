978d47ab7a1da1dd8ed2f042c0571ad35f242ec7
erikbrunno <erik.brunno@yahoo.com.br>
2019-04-04 21:43:42 -0300

    body parser

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const saudacao = require('./saudacaoMid')
 
+
+
+app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use('/saudacao', saudacao('Erik Brunno'))
 
 app.use('/opa', (req, res, next) => {
@@ -12,6 +18,19 @@ app.use('/opa', (req, res, next) => {
 
 app.use('/opa', (req, res) => {
     console.log('Durante')
+})
+
+app.get('/cliente/relatorio', (req, res) => {
+    res.send(`Cliente relatorio: completo ${req.query.completo} ano = ${req.query.ano}`)
+})
+
+app.post('/corpo', (req, res) => {
+    res.send(req.body)
+})
+
+app.get('/cliente/:id', (req, res) => {
+
+    res.send(`Cliente ${req.params.id} selecionado`)
 })
 
 app.get('/segundo', (req, res) => {
